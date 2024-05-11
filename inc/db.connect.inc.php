03b3574efe406886172fe35d22ae08f5cb45f058
Baha-Abdelatif <baha.abdelatif@gmail.com>
2024-05-11 09:13:17 +0200

    Manage file uploads and handle errors.

--- inc/db.connect.inc.php
+++ inc/db.connect.inc.php
@@ -23,7 +23,7 @@ try {
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
   ]);
 } catch (PDOException $e) {
-  // var_dump($e->getMessage());
+  var_dump($e->getMessage());
   echo 'A problem occured with the database connection...';
   die();
 }
